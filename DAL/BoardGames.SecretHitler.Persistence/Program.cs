47d9edc986873a9ced005d04962a07c74469d1db
Botirov Abdukayum <khadicharasulova@gmail.com>
2025-01-10 00:54:54 +0700

    gRpc 2 part

---
+++ DAL/BoardGames.SecretHitler.Persistence/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using BoardGames.SecretHitler.Persistence.GrpcServices;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(IPAddress.Any, 5000, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http2;
+    });
+});
+
+var app = builder.Build();
+
+app.MapGrpcService<LobbyService>();
+
+app.Run();
